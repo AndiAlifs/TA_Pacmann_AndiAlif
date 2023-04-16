64ffb6ebeb3cd2a52077733f49a8b711bc96ab8a
AndiAlifs <andyalyfsyah4@gmail.com>
2023-04-16 15:42:59 +0700

    remove env

--- env.py
+++
@@ -1,3 +0,0 @@
-JWT_SECRET_KEY = "b'\xdd\xc9\x8e\xaa_\xfd\xce\xb6S\xe2\xa3}D;a\xea'"
-DATABASE_URL = "postgresql://postgres:admin@localhost:5432/ta_paccmann"
-SECRET_KEY = "b'$U\xcae\xe9~1\xdb\xc9\xe4\x93\xf8'"
